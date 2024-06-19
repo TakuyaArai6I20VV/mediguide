61b20f16e575d6549d08876750351e383986a3d7
Takuya <158576613+TakuyaArai6I20VV@users.noreply.github.com>
2024-06-19 16:02:32 +0900

    Update firebase.tsx

--- components/firebase.tsx
+++ components/firebase.tsx
@@ -4,14 +4,7 @@ import { GoogleAuthProvider, getAuth } from "firebase/auth";
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBEfFZXLyFEU03O2y2TpH3lCxJP_PkoQ8U",
-  authDomain: "hackathon-team20.firebaseapp.com",
-  projectId: "hackathon-team20",
-  storageBucket: "hackathon-team20.appspot.com",
-  messagingSenderId: "244364540788",
-  appId: "1:244364540788:web:ca228b46917d0d386c8a21",
-  measurementId: "G-Y89DL9BK52",
-  databaseURL: "https://hackathon-team20-default-rtdb.asia-southeast1.firebasedatabase.app/"
+  ...
 };
 
 export const app = initializeApp(firebaseConfig);
