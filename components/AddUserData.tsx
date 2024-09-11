720ee26e7dfc99430dd7b8d1a44fd2565bce1c5b
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-09-11 15:47:40 +0900

    commentout

--- components/AddUserData.tsx
+++ components/AddUserData.tsx
@@ -1,23 +1,23 @@
-import { useEffect } from 'react';
-import { onAuthStateChanged } from "firebase/auth";
-import { doc, getDoc, setDoc, collection } from "firebase/firestore";
-import { auth, db } from './firebase';
+// import { useEffect } from 'react';
+// import { onAuthStateChanged } from "firebase/auth";
+// import { doc, getDoc, setDoc, collection } from "firebase/firestore";
+// import { auth, db } from './firebase';
 
-useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, async (user) => {
-        if (user) {
-            const userRef = doc(db, "Users", user.uid);
-            const docSnap = await getDoc(userRef);
-            if (!docSnap.exists()) {
-                await setDoc(userRef, {
-                    uid: user.uid,
-                    email: user.email,
-                    displayName: user.displayName
-                });
+// useEffect(() => {
+//     const unsubscribe = onAuthStateChanged(auth, async (user) => {
+//         if (user) {
+//             const userRef = doc(db, "Users", user.uid);
+//             const docSnap = await getDoc(userRef);
+//             if (!docSnap.exists()) {
+//                 await setDoc(userRef, {
+//                     uid: user.uid,
+//                     email: user.email,
+//                     displayName: user.displayName
+//                 });
                 
-                const scheduleCollectionRef = collection(userRef, "Schedule");
-            }
-        }
-    });
-    return unsubscribe;
-}, [auth, db]);
+//                 const scheduleCollectionRef = collection(userRef, "Schedule");
+//             }
+//         }
+//     });
+//     return unsubscribe;
+// }, [auth, db]);
