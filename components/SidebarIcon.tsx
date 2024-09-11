4a6e6ddd5679c917d56879dd780ebd3f2c572d1e
TakuyaArai6I20VV <leetgrow@gmail.com>
2024-09-11 18:15:03 +0900

    next.config.jsで画像読み込みを許可するドメインを指定

--- components/SidebarIcon.tsx
+++ components/SidebarIcon.tsx
@@ -1,16 +1,25 @@
 import React from 'react'
 import { auth } from './firebase'
+import Image from 'next/image'
 
 function SidebarIcon() {
   return (
-    <div className='SidebarIcon'>
-      <a href='/'>
-        <h1 className='text-3xl py-3 text-white'>MediGuide</h1>
-      </a>
-        <div className='flex items-center justify-center'>
-          <img src = {auth.currentUser?.photoURL} alt= ""/>
+    <div>
+      {auth ? (
+        <div className='SidebarIcon'>
+          <a href='/'>
+            <h1 className='text-3xl py-3 text-white'>MediGuide</h1>
+          </a>
+            <div className='flex items-center justify-center'>
+              {auth.currentUser?.photoURL && (
+                <Image src={auth.currentUser.photoURL} alt="" width={100} height={100} />
+              )}
+            </div>
+            <p>{auth.currentUser?.email}</p>
         </div>
-        <p>{auth.currentUser?.email}</p>
+      ): (
+        <div></div>
+      )}
     </div>
   )
 }
